844faef939531682c14f9aebe22e3a753a5c086e
John Nadeau <johnnnadeau@gmail.com>
2016-03-01 20:01:16 -0500

    Decompose date_time conditional

--- lib/future_appointment.rb
+++ lib/future_appointment.rb
@@ -14,13 +14,19 @@ class FutureAppointment
   def eligible_for_waitlist?
     return false if doctor != cancellation.doctor
     return false if patient == cancellation.patient
-    return false if (date_time - cancellation.date_time).to_i < 7
-    return false if (date_time - cancellation.date_time).to_i > 30
+    return false unless within_date_range?
 
     true
   end
 
   private
+
+  def within_date_range?
+    return false if (date_time - cancellation.date_time).to_i < 7
+    return false if (date_time - cancellation.date_time).to_i > 30
+
+    true
+  end
 
   attr_reader :cancellation
 end
